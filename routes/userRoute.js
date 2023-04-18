2ff261c5ab285d0d2bdf698bcb40594be30b328e
JoshuaMeguiso <joshuameguiso@gmail.com>
2023-04-18 21:33:48 +0800

    fix error 307

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -6,7 +6,7 @@ const { loginDoor, loginUser, loginUserCard, signupUser, updateUser, updateUserC
 const router = express.Router()
 
 // login door route
-router.post('/door/:room_ID', loginDoor)
+router.get('/door/:room_ID', loginDoor)
 
 // login route
 router.post('/login', loginUser)
