d16cdd2fdd7f7ce851579cf7d075e28faea8fa34
JoshuaMeguiso <joshuameguiso@gmail.com>
2024-02-23 23:04:22 +0800

    Fix

--- server.js
+++ server.js
@@ -1,36 +1,37 @@
-require('dotenv').config()
+require("dotenv").config();
 
-const express = require('express')
-const mongoose = require('mongoose')
-const userRoutes = require('./routes/userRoute')
-const tenantRoutes = require('./routes/tenantRoute')
-const roomRoutes = require('./routes/roomRoute')
-const transactionRoutes = require('./routes/transactionRoutes')
-const paymentRoutes = require('./routes/paymentRoutes')
-const tokenRoutes = require('./routes/tokenRoute')
+const express = require("express");
+const mongoose = require("mongoose");
+const userRoutes = require("./routes/userRoute");
+const tenantRoutes = require("./routes/tenantRoute");
+const roomRoutes = require("./routes/roomRoute");
+const transactionRoutes = require("./routes/transactionRoutes");
+const paymentRoutes = require("./routes/paymentRoutes");
+const tokenRoutes = require("./routes/tokenRoute");
 
 // express app
-const app = express()
+const app = express();
 
 // middleware
-app.use(express.json()) 
+app.use(express.json());
 
 // routes
-app.use('/tenant', tenantRoutes)
-app.use('/user', userRoutes)
-app.use('/room', roomRoutes)
-app.use('/transaction', transactionRoutes)
-app.use('/payment', paymentRoutes)
-app.use('/token', tokenRoutes)
+app.use("/tenant", tenantRoutes);
+app.use("/user", userRoutes);
+app.use("/room", roomRoutes);
+app.use("/transaction", transactionRoutes);
+app.use("/payment", paymentRoutes);
+app.use("/token", tokenRoutes);
 
-mongoose.connect(process.env.MONGO_URI)
+mongoose
+  .connect(process.env.MONGO_URI)
   .then(() => {
-    console.log('connected to database')
+    console.log("connected to database");
     // listen to port
     app.listen(process.env.PORT, () => {
-      console.log('listening for requests on port', process.env.PORT)
-    })
+      console.log("listening for requests on port", process.env.PORT);
+    });
   })
   .catch((err) => {
-    console.log(err)
-  }) 
+    console.log(err);
+  });
